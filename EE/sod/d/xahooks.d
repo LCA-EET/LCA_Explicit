324a26d27cd0495612ff85e5d7517c75ef9c8119
practikit <LCAMod@danielvalle.net>
2024-06-03 22:08:45 -0400

    update

--- EE/sod/d/xahooks.d
+++ EE/sod/d/xahooks.d
@@ -12,6 +12,8 @@ APPEND BDCORWIJ
 	IF ~~ THEN BEGIN XA_Explicit_0000
 		SAY @1 /* ~Something I know you'll enjoy. Follow me.~ */
 		
+		= @5 /*~(She takes hold of your hand and leads you through the gathered soldiers and officers, many of whom take a moment to congratulate the two of you on your hard-earned victory. Raucous cheering erupts as you hear corks popping all around you, and you and Schael — despite your best efforts — are showered in some very expensive spirits. In the midst of the revelry, you can hear the quartermaster announce that the barrels were rolled in and the spigots are open. While the men and women of the coalition are busy queueing up for their ale, Schael quickly and discreetly leads you into one of the castle's storerooms...)~*/
+		
 		IF ~~ THEN 
 		DO ~
 			StartCutSceneMode()
